57395c21785f53e4100bb0ea181f25cd44b8c7aa
CURigel <rigel.cyrosea@gmail.com>
2016-02-29 18:26:56 +0000

    Add layout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 end
